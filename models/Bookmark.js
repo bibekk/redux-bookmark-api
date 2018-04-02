4c13b7e61e353863e66910bdd41fdf67bfc32473
bibekk <bibek.k@gmail.com>
2018-04-01 22:01:04 -0700

    added sort by on queries

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,13 +1,13 @@
 var db = require('../dbconnection');
 var Bookmark ={
   getAllBookmarks: function(callback){
-      return db.query("SELECT b.id, b.url ,a.cat_id, c.category FROM db_bookmark.tbl_bookmarks  b join tbl_categories c USING(cat_id) ",callback);
+      return db.query("SELECT b.id, b.url ,a.cat_id, c.category FROM db_bookmark.tbl_bookmarks  b join tbl_categories c USING(cat_id) order by c.category",callback);
   },
   getBookmarkCategory:function(callback){
-      return db.query("SELECT  c.cat_id,c.category, COUNT(b.id) AS total FROM tbl_bookmarks b RIGHT JOIN tbl_categories c USING(cat_id) GROUP BY cat_id;",callback);
+      return db.query("SELECT  c.cat_id,c.category, COUNT(b.id) AS total FROM tbl_bookmarks b RIGHT JOIN tbl_categories c USING(cat_id) GROUP BY cat_id order by c.category;",callback);
   },
   getBookmarksByCategory: function(cat_id,callback){
-     return db.query("SELECT a.id, a.url,a.cat_id, b.category from tbl_bookmarks a JOIN tbl_categories b using(cat_id) where cat_id=?",[cat_id],callback);
+     return db.query("SELECT a.id, a.url,a.cat_id, b.category from tbl_bookmarks a JOIN tbl_categories b using(cat_id) where cat_id=? order by b.category",[cat_id],callback);
   },
   addBookmark: function(body,callback) {
       return db.query("insert into tbl_bookmarks(url,cat_id) values('"+body.url+"',"+body.cat_id+")",callback)
