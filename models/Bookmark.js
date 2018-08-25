3fe2f7d1b2682648ad32851071130443809c6447
bibekk <bibek.k@gmail.com>
2018-08-24 22:12:07 -0700

    order by id desc for latest

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -7,7 +7,7 @@ var Bookmark ={
       return db.query("SELECT  c.cat_id,c.category, COUNT(b.id) AS total FROM tbl_bookmarks b RIGHT JOIN tbl_categories c USING(cat_id) GROUP BY cat_id order by c.category;",callback);
   },
   getBookmarksByCategory: function(cat_id,callback){
-     return db.query("SELECT a.id, a.url,a.cat_id, b.category from tbl_bookmarks a JOIN tbl_categories b using(cat_id) where cat_id=? order by b.category",[cat_id],callback);
+     return db.query("SELECT a.id, a.url,a.cat_id, b.category from tbl_bookmarks a JOIN tbl_categories b using(cat_id) where cat_id=? order by a.id desc",[cat_id],callback);
   },
   addBookmark: function(body,callback) {
       return db.query("insert into tbl_bookmarks(url,cat_id) values('"+body.url+"',"+body.cat_id+")",callback)
